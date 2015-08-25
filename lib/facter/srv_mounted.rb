e03c751e7831e80103ef3bb57ca902e4bb851aad
Jan Skoda <skoda@jskoda.cz>
2015-08-25 14:04:08 +0200

    srv_mounted fact using lost+found

--- lib/facter/srv_mounted.rb
+++ lib/facter/srv_mounted.rb
@@ -1,13 +1,6 @@
 Facter.add('srv_mounted') do
   setcode do
-    case Facter::Core::Execution.exec('/bin/mountpoint /srv')
-        when 0
-                true
-        when 1
-                false
-        else    
-                false
-        end
+    File.exist? '/srv/lost+found'
   end
 end
 
